ea712630458d7ca52a6be75f8a3f789a085e88ed
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-07 17:12:34 +0300

    added type annotation for task 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> [float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     function for creating a generator.
     """
