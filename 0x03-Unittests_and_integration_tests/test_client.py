a28811a3c7a28ae1d83ea4ae3f1792e83186497e
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-26 13:58:38 +0300

    license check

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -49,3 +49,14 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(repos, ["gmail", "google_drive"])
             mocked.assert_called_once()
             mocked_get.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+        ])
+    def test_has_license(self, repo, licence_key, expected):
+        """test for licence"""
+        self.assertEqual(
+            GithubOrgClient.has_license(repo, licence_key),
+            expected
+            )
